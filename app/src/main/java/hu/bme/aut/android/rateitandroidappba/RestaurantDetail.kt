42299d19ee4cf0128bbe26eabfffc463c796c90f
BAttila00 <bruderattila@gmail.com>
2020-12-03 18:27:55 +0100

    restaurant detail layout FloatingActionButton click listener

--- app/src/main/java/hu/bme/aut/android/rateitandroidappba/RestaurantDetail.kt
+++ app/src/main/java/hu/bme/aut/android/rateitandroidappba/RestaurantDetail.kt
@@ -1,7 +1,10 @@
 package hu.bme.aut.android.rateitandroidappba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_restaurant_detail.*
 
 class RestaurantDetail : AppCompatActivity() {
@@ -20,6 +23,11 @@ class RestaurantDetail : AppCompatActivity() {
         tvRestaurantAddress.text = intent.getStringExtra(KEY_ADDRESS)
         tvRestaurantURL.text = intent.getStringExtra(KEY_URL)
 
+        val fab: FloatingActionButton = findViewById(R.id.fabResDetail)
 
+        //add restaurant posts button listener
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext,"fab pushed",Toast.LENGTH_SHORT).show()
+        }
     }
 }
